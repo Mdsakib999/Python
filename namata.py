b50a43b8555106ea910f02e3afa9af83b5197543
Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-08-08 22:53:23 +0600

    Add files via upload

---
+++ namata.py
@@ -0,0 +1,18 @@
+x = int(input("Enter a number : "))
+
+for i in range(1, 11):
+    #a = x * i
+    
+    print(x, "x", i, "=", x*i)  # x*i ar jaigai a deao kora jabe
+    i += 1
+
+
+"""
+usuing while loop
+
+n = 3
+i = 1
+while i < 11:
+    print(n, "x", i, "=", n*i)
+    i += 1
+"""
