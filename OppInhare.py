8795d2eaef30ac32879b95dabc8ae54c8e366935
Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-08-09 21:53:26 +0600

    Add files via upload

---
+++ OppInhare.py
@@ -0,0 +1,31 @@
+#inharetace in python
+
+class Vhecale:
+    def __init__(self, cost, milage):
+        self.cost = cost
+        self.milage = milage
+
+    def show_dls(self):
+        print("cost is", self.cost)
+        print("milage is", self.milage)
+        print("I am a vhecile")
+
+v1= Vhecale(100, 5000)
+v1.show_dls()
+
+        #child class inharat 
+class Car(Vhecale):
+    def __init__(self, cost, milage, color, hp):
+        super().__init__(cost, milage)
+        self.color = color
+        self.hp = hp
+
+
+    def show_dls_car(self):
+        print("I am car")
+        print("Colorr of car", self.color)
+        print("hourse power", self.hp)
+        
+c1= Car(200, 8000, "blue", 32)
+c1.show_dls()
+c1.show_dls_car()
