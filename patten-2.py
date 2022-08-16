80f37b24d6d310e27bb81aabcf843ea484de6e56
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-08-16 21:56:58 +0600

    Add files via upload

---
+++ patten-2.py
@@ -0,0 +1,10 @@
+
+n = 5
+for i in range(n):
+    for j in range(i, n):        #(i, n) to see reverse (star ar jaigai space dita hobe)
+        print(" ", end=" ")
+    for j in range(i+1):
+        print("*", end=" ")     #(i+1)
+
+    print()
+
