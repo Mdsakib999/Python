05cb5a20cd9fcc7e3b62dba054931b92a5b52669
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-08-20 13:19:11 +0600

    Add files via upload

---
+++ practiceOpp1.py
@@ -0,0 +1,16 @@
+#opp practice (calculate the aria of Triangle)
+
+class Triangle:
+    def __init__(self, base, hight):
+        self.base = base
+        self.hight = hight
+
+    def cal_aria(self):
+        area = 0.5 * self.base * self.hight
+        print("Aria of tringle", area)
+        
+t1 = Triangle(20, 30)
+t1.cal_aria()
+
+t2 = Triangle(10, 20)
+t2.cal_aria()
