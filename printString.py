7919bab6bf38c8753d561a9d27c28f7fe64e9319
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-08-20 13:20:34 +0600

    Add files via upload

---
+++ printString.py
@@ -0,0 +1,10 @@
+#udacity DSA course problems solution
+
+def show_excitement():
+    # Your code goes here!
+    for i in range(1,5):
+        print("I am super excited for this course!", end=" ")
+        i += 1
+    return "I am super excited for this course!"
+            
+print (show_excitement())
