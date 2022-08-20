68bde115732fea90e736a7c522cb33cc11ef1597
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-08-20 13:18:52 +0600

    Add files via upload

---
+++ positive&nega.py
@@ -0,0 +1,10 @@
+#check positive orr negative number.
+
+num = int(input("Enter a number :"))
+
+if num > 0:
+    print("Its a positive number!!!")
+elif num == 0:
+    print("Its zero")
+else:
+    print("Its a negative number!!!")
