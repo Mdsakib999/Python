078dfb0c27d1eee583fd5fb43f974350ee484dd9
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-08-22 21:54:51 +0600

    Add files via upload

---
+++ sqrRoot.py
@@ -0,0 +1,17 @@
+#Sqe root of complex nuberr
+
+import cmath
+
+numbr = 1 + 10j
+
+num_sqr = cmath.sqrt(numbr)
+
+print("the sqr root od {0} is {1:0.3f}+{2:0.3f}j".format(numbr,num_sqr.real,num_sqr.imag))
+
+"""
+a = int(input("enterr a :"))
+b = int(input("enterr b :"))
+c = a + b
+print("thr sum of {1} and {0} is {2}".format(a,b,c))
+print("sum of", a, "and", b, "is", c)
+"""
