1d78139a7e55d94889848a3e02d437c9d64b5255
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-08-31 22:55:07 +0600

    Add files via upload

---
+++ problems2.py
@@ -0,0 +1,11 @@
+
+#Python program to find the euclidean distance
+
+x1 = float(input("Enter x1 of x cordenate :"))
+y1 = float(input("Enter y1 of y cordenate :"))
+x2 = float(input("Enter x2 of x cordenate :"))
+y2 = float(input("Enter y2 of y cordenate :"))
+
+result = ((x2 - x1)**2 + (y2 - y1)**0.5)
+
+print(result)
