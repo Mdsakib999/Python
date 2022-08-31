5aba678426abd1307d4d11002c355afe9a6b5a0f
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-08-31 23:00:43 +0600

    Add files via upload

---
+++ problems4.py
@@ -0,0 +1,11 @@
+#calculate profit or loss of any product
+
+a = float(input('Enter buying cost :'))
+b = float(input('Enter selling cost :'))
+
+if a < b:
+    amount = b - a
+    print ('profit', amount)
+else:
+    amount = a - b
+    print('loss', amount)
