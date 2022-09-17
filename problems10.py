82d83ca392bed074da10b027581757d5703ed3fa
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-09-17 23:09:43 +0600

    Add files via upload

---
+++ problems10.py
@@ -0,0 +1,15 @@
+#Find rots of a Quadratic Equation(ax**2 + bx + c = 0)
+
+import cmath
+
+a = 50
+b = 60
+c = 40
+#calculate the discriminant
+
+d = (b**2) - (4*a*c)
+
+#find two solution
+r1 =(-b-cmath.sqrt(d))/(2*a)
+r2 =(-b+cmath.sqrt(d))/(2*a)
+print("Roots are :", r1, r2)
