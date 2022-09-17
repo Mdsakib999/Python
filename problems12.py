e8c1b8fad1ea13ad43e735ef306e697852aadc59
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-09-17 23:11:38 +0600

    Add files via upload

---
+++ problems12.py
@@ -0,0 +1,28 @@
+#Fibonacci Sequence (sum of first two number)
+
+#nterm = -5
+nterm = int(input("Enter a number :"))
+
+n1 = 0
+n2 = 1
+sum = 0
+
+if nterm <= 0:
+    print("Enter a positv number")
+
+elif nterm == 1:
+    print("Fibonacci sequence upto", nterm)
+    print(n2)
+
+else:
+    print("Fibonacci sequence upto", nterm)
+
+    while sum < nterm:
+        print(n1, end=", ")
+        nth = n1 + n2
+
+        n1 = n2
+        n2 = nth
+        sum += 1
+
+print()
