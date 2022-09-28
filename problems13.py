91d7e9039df3847be7a4e9ecdb286f9cfac9f762
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-09-28 21:28:47 +0600

    Add files via upload

---
+++ problems13.py
@@ -0,0 +1,15 @@
+# Floor division
+
+a = int(input("Enter a :"))
+b = int(input("Enter b :"))
+
+c = a//b        #c = math.floor(a/b)
+
+print("Floor divission", c)
+
+"""
+import math
+#num = float(input("enter a float number"))
+result = math.floor(3.4)       #num
+print(result)
+"""
