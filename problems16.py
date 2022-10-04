6b2856b3d1a272174925f67bc81358eca93f2faf
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-10-04 21:21:16 +0600

    Add files via upload

---
+++ problems16.py
@@ -0,0 +1,32 @@
+#Sum and Average of given number
+
+len = int(input("How many number do you want to enter : "))
+num = []
+
+for i in range(0, len):
+    ele = int(input("Enter element : "))
+    num.append(ele)
+
+sum_num = sum(num)
+print("sum of number : ", sum_num)
+
+avg = sum_num/len
+print("Average", round(avg,2))
+
+
+
+
+
+"""
+len = int(input("how many number want to adde : "))
+
+sum = 0
+
+for i in range(0, len):
+    ele = int(input("Enter element : "))
+    sum += ele
+
+print("Sum =", sum)
+avg = sum / len
+print("Average =", avg)
+"""
