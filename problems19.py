9694c5616505e9de0c45e0a3d4829efd001374a5
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-10-06 21:39:28 +0600

    Add files via upload

---
+++ problems19.py
@@ -0,0 +1,17 @@
+#Check Armstrong number or not.
+
+userInput = int(input("Enter your number : "))      #enter 3 digit number
+nums = userInput
+
+a = nums % 10
+nums = nums // 10
+b = nums % 10
+c = nums // 10
+
+if (a**3)+(b**3)+(c**3) == userInput:           #formula
+    print(userInput, "is a Armstrong number")
+else:
+    print(userInput, "is not a Armstrong number")
+
+
+#if we take power 4 it will Narcissictic number (a**4)
