a575d6c7638c97ef4d348570315d60632371d096
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-10-06 21:39:11 +0600

    Add files via upload

---
+++ problems18.py
@@ -0,0 +1,14 @@
+#Find the largest number of list
+
+def large_num(nums):
+    largest = nums[0]
+    for i in nums:
+        if largest < i:
+            largest = i
+    return largest
+
+nums = [9,40,25,8,30]
+
+largest = large_num(nums)       #largest = max(nums) (shortcut)
+
+print("Large of the list is : ", largest)
