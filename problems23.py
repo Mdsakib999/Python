40c9158d7957d3cfbf729027ed13ff1328ad0bd3
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-10-09 21:57:53 +0600

    Add files via upload

---
+++ problems23.py
@@ -0,0 +1,14 @@
+#factorial of given number of user
+
+num = int(input("Enter number : "))
+user_inp = num
+i = 1
+
+if num >= 0:
+    while num >= 1:
+        i = i * num
+        num -= 1
+    print("Factorial of",user_inp, "is", i)
+
+else:
+    print("enter a positive number")
