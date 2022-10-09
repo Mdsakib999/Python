a294d8c3af2805af1a6160068a2efff8a7877741
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-10-09 21:59:00 +0600

    Add files via upload

---
+++ problems25.py
@@ -0,0 +1,15 @@
+#OPPs problems solution (calculate discount)
+
+class Laptop:
+    def __init__(self, name, brand, price):
+        self.name = name
+        self.brand = brand
+        self.price = price
+
+    def cal_discount(self, prcentage):
+        offfer_price = (prcentage/100)*self.price
+        return self.price - offfer_price
+
+laptop1 = Laptop("Core i5", "Dell", 55000)
+print("Price after discount ",laptop1.cal_discount(5))
+      
