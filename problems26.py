3dadda3c31de36081b956ce578b5e436956c8f63
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-10-09 21:59:25 +0600

    Add files via upload

---
+++ problems26.py
@@ -0,0 +1,21 @@
+# print Fibonacci Sequence
+
+number = int(input("Enter number : "))
+
+n1 = 0
+n2 = 1
+count = 0
+
+if number <= 0:
+    print("Enter positive number : ")
+
+else:
+    while count <= number:
+        print(n1, end=", ")
+        nth = n1 + n2
+
+        n1 = n2
+        n2 = nth
+        count += 1
+
+print()
