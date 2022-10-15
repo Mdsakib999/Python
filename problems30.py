7d73a93f9275e51751f7e37d4a9078e1f363aa2a
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-10-15 23:59:08 +0600

    Add files via upload

---
+++ problems30.py
@@ -0,0 +1,19 @@
+#display the power of given number range
+
+num = 10        #can take input from usear 
+
+result = list(map(lambda X: 2**X, range(num+1))) #important
+
+print("total terms is : ", num)
+
+for i in range(num+1):
+    print("2 rised to power", i, "is", result[i])
+
+
+
+"""
+num = 5
+
+for i in range(num):
+    print("2 terms of power", i, "is", i**2)
+"""
