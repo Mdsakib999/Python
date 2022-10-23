91cbd1e5ef9db436e74fad894689027758ad07f9
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-10-23 23:58:58 +0600

    Add files via upload

---
+++ problems33.py
@@ -0,0 +1,10 @@
+#decimal to octal, binary and haxadecimal
+
+from unicodedata import decimal
+
+
+num = int(input("Enter a decimal number : "))
+
+print(bin(num),"in bianry")
+print(oct(num),"in Octal")
+print(hex(num),"in haxadecimal")
