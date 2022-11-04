ace5ce2c6ae4364ebe8de57a5b8f12336f946ea2
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-11-04 23:32:47 +0600

    Add files via upload

---
+++ problems40.py
@@ -0,0 +1,13 @@
+#Find Missing Number in a list
+
+def findMissingNumbers(n):
+    numbers = set(n)
+    length = len(n)
+    output = []
+    for i in range(1, length + 1):
+        if i not in numbers:
+            output.append(i)
+    return output
+    
+listOfNumbers = [1, 2, 3, 5, 7, 8, 9]
+print(findMissingNumbers(listOfNumbers))
