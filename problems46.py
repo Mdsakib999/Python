6371358ab660ea1644c9718b22c481d8f85da47d
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-11-16 23:21:08 +0600

    Add files via upload

---
+++ problems46.py
@@ -0,0 +1,12 @@
+#the two sum problem using python? if a + b = g. return len(a,b)
+
+def twosum(nums, target):
+    length = len(nums)
+    for i in range(length):
+        for j in range(i + 1, length):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+
+n = [3, 1, 1, 2]
+t = 5
+print(twosum(n, t))
