5d8a381f7ce2e98c7121159513ec8f5d9f68650b
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-11-22 21:37:20 +0600

    Add files via upload

---
+++ problems51.py
@@ -0,0 +1,17 @@
+#calculate the execution time of python program?
+
+from time import time
+start = time()
+
+#create acronyms prigram
+word = "Sakib Vai"
+text = word.split()
+a = " "
+
+for i in text:
+    a = a+str(i[1]).upper()
+print(a)
+
+end = time()
+exe_time = end - start
+print("Execution time ", exe_time)
