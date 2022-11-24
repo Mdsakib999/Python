a03d0e5452b51cecfa55f95003ba47c692b118fa
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-11-24 23:52:19 +0600

    Add files via upload

---
+++ problems52.py
@@ -0,0 +1,39 @@
+#Find last common multiple(LCM) of two number???
+
+def lcm(x, y):
+    if x > y:
+        greater = x
+    else:
+        greater = y
+    while(True):
+        if((greater % x == 0) and (greater % y == 0)):
+            lcm = greater
+            break
+        greater += 1
+    return lcm
+
+num1 = 54
+num2 = 24
+print("The LCM of", num1, "and", num2,"is : ", lcm(num1, num2))
+
+
+
+
+"""
+def gcd(x, y):
+
+    while(y):
+        x,y = y, x % y
+    return x
+
+def lcm(x, y):
+    lcm = (x * y) // gcd(x, y)
+    return lcm
+
+
+n1 = 54
+n2 = 24
+
+print(lcm(n1, n2))
+
+"""
