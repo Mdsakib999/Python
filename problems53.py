7070ea315131fe832cd8da34488f05af6eb794e1
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-11-24 23:53:34 +0600

    Add files via upload

---
+++ problems53.py
@@ -0,0 +1,13 @@
+#Shuffle Duck of card using random module...?
+
+import itertools, random
+
+deck = list(itertools.product(range(1, 14), ["Spade", "Heart", "Diamond", "Club"]))
+
+random.shuffle(deck)
+
+#draw 5 cards
+print("You got :")
+for i in range(5):      #I can increase changing range value
+    
+    print(deck[i][0], "of", deck[i][1])
