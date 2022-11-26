79ada8e808eb538830038339faec8e8d0607c90a
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-11-26 21:49:48 +0600

    Add files via upload

---
+++ problems54.py
@@ -0,0 +1,8 @@
+#Write a program to display a month of a year given by user..
+
+import calendar
+
+yy = int(input("Enter the year: "))
+mm = int(input("Enter the month: ")) 
+dd = 9                                  # Also can print without dd
+print(calendar.month(yy, mm, dd))
