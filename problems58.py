b9b3cbe70d7d27820f9d1730ef1dede366dcfeb4
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-12-02 23:25:37 +0600

    Add files via upload

---
+++ problems58.py
@@ -0,0 +1,12 @@
+#Convert Decimal to Binary using Recursion
+
+def dec_to_bin(n):
+    if n > 1:
+        dec_to_bin(n//2)
+    print(n % 2, end="")
+
+
+dec = int(input("Enter a Decimal number : "))        #3.2, 50.2 float number is not possible to convert
+
+dec_to_bin(dec)
+print()
