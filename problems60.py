43b57c896539409fea153f137bcde0be849653fb
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-12-13 23:34:09 +0600

    Add files via upload

---
+++ problems60.py
@@ -0,0 +1,27 @@
+#Transpose Matrix in Python..
+
+#using nested loop
+x = [[12, 7], [4, 5], [3, 8]]
+
+result = [[0,0,0],[0,0,0]]
+
+for i in range(len(x)):
+    for j in range(len(x[0])):
+        result[j][i] = x[i][j]
+
+for a in result:
+    print(a)
+
+
+
+
+
+"""
+x = [[12, 7], [4, 5], [3, 8]]
+
+result = [[x[j][i] for j in range(len(x))] for i in range(len(x[0]))]
+
+for r in result:
+   print(r)
+
+"""
