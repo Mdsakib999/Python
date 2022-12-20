981321c847ad100019154d69b4089d449a1773b8
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-12-20 23:29:29 +0600

    Add files via upload

---
+++ problems61.py
@@ -0,0 +1,15 @@
+#Multiply tow Matrices...
+
+x = [[5, 8, 16], [8, 10, 3], [7,5,22]]
+y = [[2, 7, 6], [18, 1, 9], [5,15,2]]
+
+result = [[0,0,0], [0,0,0], [0,0,0]]
+
+for i in range(len(x)):
+    for j in range(len(y[0])):
+        for k in range(len(y)):
+            result[i][j] += x[i][k] * y[k][j]
+
+
+for r in result:
+    print(r)
