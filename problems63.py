2b9c57ec92fc0cb1fc67241de363a09622c8dfc8
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2022-12-23 21:49:09 +0600

    Add files via upload

---
+++ problems63.py
@@ -0,0 +1,16 @@
+#Remove punctuation from string.
+
+from string import punctuation
+
+
+#punctuation = """!@#$%&*()_+:\;,./{?}[]"'"""
+
+myString = "hi, how are u?... i'm fine()"
+
+no_punc = ""
+
+for char in myString:
+    if char not in punctuation:
+        no_punc = no_punc + char
+
+print(no_punc)
