7885ded32d96ad8e20824e46c26ae0a1f618b0eb
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-01-08 22:47:17 +0600

    Add files via upload

---
+++ problems67.py
@@ -0,0 +1,32 @@
+#Simple Interest counter.
+
+main_amount = int(input("How money you borrowed? "))
+interest_rate = float(input("Interest rate: "))
+time = float(input("Total time in year: "))
+
+interest = main_amount * (interest_rate / 100) * time
+
+print("Simple Interest is", interest)
+
+total_amount = main_amount + interest
+
+print("Total amount with Interest is", total_amount)
+
+
+
+
+
+
+
+
+"""
+#Compound interest counter......
+
+main_amount = int(input("How money you borrowed? "))
+interest_rate = float(input("Interest rate: "))
+time = float(input("Total time in year: "))
+
+interest = main_amount * ((1 + interest_rate/100)**time)
+
+print("compound interest is: ", interest)
+"""
