b6d894bd1b9dbab21bf5bcd92ce8ac4e5b2b6c0c
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-06-16 22:28:12 +0600

    commit

--- KmToMile.py
+++
@@ -1,6 +0,0 @@
-#kilometers to miles
-kilo = float(input("Enter kelometers :"))
-
-miles = kilo * 0.621371
-
-print(kilo,"Km is equle to %0.2f" %(miles), "miles")
---
+++ problems83.py
@@ -0,0 +1,9 @@
+#Q: Convert kilometers to miles in Python.....?
+
+
+
+kilo = float(input("Enter kelometers :"))
+
+miles = kilo * 0.621371
+
+print(kilo,"Km is equle to %0.2f" %(miles), "miles")
