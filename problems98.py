bd87f87161cde7a00f2e1cfb3d40031a18034ee2
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-06-27 16:29:00 +0600

    commit

--- problems98.py
+++ problems98.py
@@ -1,8 +1,10 @@
+# Printing patten
+
 n = 5
 
 for i in range(n):
     for j in range(i, n):  # (i+1) it will revers        
-        print("*", end=" ")
+        print("*", end=" ");
 
 
-    print()
+    print();
