7dcdc97bf64a767c4f72660bc539f542d7ded518
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-07-21 23:12:12 +0600

    commit

--- problems14.py
+++ problems14.py
@@ -1,7 +1,7 @@
-# Date and time with local standard
+# Date and time with local standard?
 
 
 import time
 tim = time.localtime();
 
-print(time.strftime("%a %b %d %Y %H:%M:%S %Z", tim))
+print(time.strftime("%a %b %d %Y %H:%M:%S %Z", tim));
