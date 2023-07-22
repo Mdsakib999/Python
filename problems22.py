c34522d324d83a7acdfdc33b928da61c5dd3fee7
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-07-23 00:23:49 +0600

    commit

--- problems22.py
+++ problems22.py
@@ -1,7 +1,7 @@
-#print possible number combinations
+#print possible number combinations.
 
 
 for i in range(1,5):
     for j in range(1,5):
         if (i != j):
-            print(i,j)
+            print(i,j);
