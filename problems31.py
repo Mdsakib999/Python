e58feb19ab25928bf462486c53910b52b1151776
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-07-31 23:42:41 +0600

    commit

--- problems31.py
+++ problems31.py
@@ -1,9 +1,9 @@
-#Findd number divisible by giver number in list
+#Q: Findd number divisible by giver number in list??
 
 my_list = [5, 10, 12, 30, 40]
 
-num = 5
+num = 5;
 
-result = list(filter(lambda X:(X % num ==0), my_list))
+result = list(filter(lambda X:(X % num ==0), my_list));
 
-print("Numbers divisible by",num,"are",result)
+print("Numbers divisible by",num,"are",result);
