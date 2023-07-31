215e9925f2118d9d00187f920ccad641c9d96e68
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-07-31 23:35:51 +0600

    commit

--- problems3.py
+++ problems3.py
@@ -1,10 +1,10 @@
-#take 3 angle from user and check it possible to make triangle or not???
+#Take 3 angle from user and check it is possible to make triangle or not???
 
-a = int(input("Enter angle a :"))
-b = int(input("Enter angle b :"))
-c = int(input("Enter angle c :"))
+a = int(input("Enter angle a :"));
+b = int(input("Enter angle b :"));
+c = int(input("Enter angle c :"));
 
 if a + b + c ==180 and a!=0 and b!=0 and c!=0:
-    print("possible to make triangle")
+    print("possible to make triangle");
 else:
-    print("Not possible")
+    print("Not possible");
