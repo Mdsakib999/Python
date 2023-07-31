d6fb61fa597fc1e91ab3b5b9dbc0f965021c274a
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-07-31 23:37:08 +0600

    commit

--- problems30.py
+++ problems30.py
@@ -1,13 +1,13 @@
-#display the power of given number range
+# Display the power of given number range??
 
 num = 10        #can take input from usear 
 
 result = list(map(lambda X: 2**X, range(num+1))) #important
 
-print("total terms is : ", num)
+print("total terms is : ", num);
 
 for i in range(num+1):
-    print("2 rised to power", i, "is", result[i])
+    print("2 rised to power", i, "is", result[i]);
 
 
 
@@ -15,5 +15,5 @@ for i in range(num+1):
 num = 5
 
 for i in range(num):
-    print("2 terms of power", i, "is", i**2)
+    print("2 terms of power", i, "is", i**2);
 """
