c9b81523c5208142b8b16864c6c59efeb3926a2f
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-08-07 23:59:44 +0600

    commit

--- problems34.py
+++ problems34.py
@@ -1,6 +1,7 @@
 #Find the ascii value of character!!!
-
 #a = "s"
-a = input("Enter a character : ")
 
-print("The ascii valu of", a, "is", ord(a))
+
+a = input("Enter a character : ");
+
+print("The ascii valu of", a, "is", ord(a));
