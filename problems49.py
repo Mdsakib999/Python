8ff6aa5f24cc0cd10e78d128729f1d48fd73f93f
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-09-02 23:09:33 +0600

    commit

--- problems49.py
+++ problems49.py
@@ -9,6 +9,7 @@ def maximum(x):
         current_index = current_index + 1
     return maximum_index
 
-a = [76, 45, 20, 70, 65, 150, 54]
-print("Maximum value index", maximum(a))
-print("Maximum value of list", max(a))
+
+a = [76, 45, 20, 70, 65, 150, 54];
+print("Maximum value index", maximum(a));
+print("Maximum value of list", max(a));
