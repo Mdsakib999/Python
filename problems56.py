b6c5e1a6caadd6e583f1171594623f158a077e1b
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-09-02 23:22:07 +0600

    commit

--- problems56.py
+++ problems56.py
@@ -2,13 +2,13 @@
 
 def rec_sum(n):
     if n <= 1:
-        return n
+        return n;
     else:
         return n + rec_sum(n - 1)
 
-num = 10
+num = 10;
 
 if num <= 0:
-    print("Enter a positive number :")
+    print("Enter a positive number :");
 else:
-    print("The sum is:", rec_sum(num))
+    print("The sum is:", rec_sum(num));
