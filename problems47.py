dbae4743961c8037cb5b1f80396a6e6980e363d5
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-09-02 23:08:16 +0600

    commit

--- problems47.py
+++ problems47.py
@@ -9,6 +9,6 @@ with open("blog.html", "r") as f:
         words.extend(line.split())
 
 from collections import Counter
-counts = Counter(words)
-top5 = counts.most_common(5)
-print(top5)
+counts = Counter(words);
+top5 = counts.most_common(5);
+print(top5);
