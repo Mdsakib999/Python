2de33ef12becd68e2d52efd2810085b2a3a24ab8
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-09-02 23:20:14 +0600

    commit

--- problems55.py
+++ problems55.py
@@ -1,8 +1,9 @@
 #problems12 
 #Fibonacci Sequence using function
 
+
 def re_fibo(n):
-    if n <= 1:          #if you want see negative value use n < 1
+    if n <= 1:                     #if you want see negative value use n < 1
         return n
     else:
         return(re_fibo(n-1) + re_fibo(n-2))
@@ -12,6 +13,6 @@ nth = 10
 if nth <= 0:
     print("Enter a positive number :")
 else:
-    print("Fibonacci seq: ")
+    print("Fibonacci seq: ");
     for i in range(nth):
-        print(re_fibo(i)) #end=", "
+        print(re_fibo(i))           #end=", "
