ca44b09875095f0c25f052ba7cf82840c7e2a962
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-09-02 23:19:17 +0600

    commit

--- problems54.py
+++ problems54.py
@@ -2,7 +2,7 @@
 
 import calendar
 
-yy = int(input("Enter the year: "))
-mm = int(input("Enter the month: ")) 
+yy = int(input("Enter the year: "));
+mm = int(input("Enter the month: ")); 
 dd = 9                                  # Also can print without dd
-print(calendar.month(yy, mm, dd))
+print(calendar.month(yy, mm, dd));
