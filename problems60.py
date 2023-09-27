2211ea537bb74c55bb5bffebb2653630bf1a50d8
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-09-27 23:55:48 +0600

    commit

--- problems60.py
+++ problems60.py
@@ -10,18 +10,18 @@ for i in range(len(x)):
         result[j][i] = x[i][j]
 
 for a in result:
-    print(a)
+    print(a);
 
 
 
 
-
+# 2nd way
 """
 x = [[12, 7], [4, 5], [3, 8]]
 
 result = [[x[j][i] for j in range(len(x))] for i in range(len(x[0]))]
 
 for r in result:
-   print(r)
+   print(r);
 
 """
