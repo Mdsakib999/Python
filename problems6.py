c4d7ba0a20a14a5fcba084d0b91c64605a29770d
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-09-27 23:54:08 +0600

    commit

--- problems6.py
+++ problems6.py
@@ -1,11 +1,11 @@
 #vowle count
 
 def vou_con(sentence):
-    vowle = ['a', 'e', 'i','o', 'u', 'A','E', 'I', 'O', 'U']
+    vowle = ['a', 'e', 'i','o', 'u', 'A','E', 'I', 'O', 'U'];
     count = 0
     for cher in sentence:
         if cher in vowle:
             count += 1
     return count
 
-print(vou_con('hi ame tmak valobase'))
+print(vou_con('hi ame tmak valobase'));
