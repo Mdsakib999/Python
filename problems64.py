fef1f88bc7300baf76c66ba9e70adf798b6bbb4c
MD Sakib <89766088+Mdsakib999@users.noreply.github.com>
2023-10-07 19:16:45 +0600

    commit

--- problems64.py
+++ problems64.py
@@ -12,9 +12,10 @@ print("the sqr root od {0} is {1:0.3f}+{2:0.3f}j".format(numbr,num_sqr.real,num_
 
 #Another way
 """
-a = int(input("enterr a :"))
-b = int(input("enterr b :"))
+a = int(input("enterr a :"));
+b = int(input("enterr b :"));
 c = a + b
-print("thr sum of {1} and {0} is {2}".format(a,b,c))
-print("sum of", a, "and", b, "is", c)
+print("thr sum of {1} and {0} is {2}".format(a,b,c));
+print("sum of", a, "and", b, "is", c);
+
 """
